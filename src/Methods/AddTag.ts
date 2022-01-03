dc0466d3b532cdf476a656f2fbe8c5f5dbe80286
Simon Skinner <vultuk28@gmail.com>
2022-01-03 09:13:36 +0000

    Add tag extracted

--- src/Methods/AddTag.ts
+++ src/Methods/AddTag.ts
@@ -1,7 +1,9 @@
-import { ApiCallMethod } from "../Types";
+import { ApiCallMethod } from '../Types';
 
-const addTag = (call: ApiCallMethod) => (userId: string, tag: string) => call<{subscriber_id: string; tag_name: string}>('', {
-  subscriber_id: userId, tag_name: tag
-})
+const addTag = (call: ApiCallMethod) => (userId: string, tag: string) =>
+  call<{ subscriber_id: string; tag_name: string }>('fb/subscriber/addTagByName', {
+    subscriber_id: userId,
+    tag_name: tag,
+  });
 
 export default addTag;
